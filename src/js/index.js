6f45c1a4d3a79605791916b106a6861b53161250
devmango1128 <devmango1128@gmail.com>
2024-10-06 04:53:26 +0900

    앱 적용 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1159,7 +1159,7 @@ function startBackup() {
             }
 
             const blob = new Blob([JSON.stringify(backup)], { type: 'application/json' });
-            const fileName = `mapApp_backup.json`;
+            const fileName = `mapApp_backup_${formatDate(new Date())}.json`;
 
             if (window.Android) {
                 const base64Data = await blobToBase64Android(blob);
@@ -1171,6 +1171,17 @@ function startBackup() {
             saveBackupInfo(blob.size, fileName);
         };
     };
+}
+
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
 }
 
 function blobToBase64(blob) {
